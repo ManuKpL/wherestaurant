db673511de1f307acdaa5e8b6fb7a91b446199a3
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-19 12:27:17 +0200

    added the validations of the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  validates_presence_of :content, :rating, :restaurant_id
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 6 }
 end
