3216bbbab44f457808803385801338a93535c640
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2015-08-19 18:59:07 +0200

    added some style + picture in restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819094947) do
+ActiveRecord::Schema.define(version: 20150819163920) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150819094947) do
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "picture"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,25 +12,29 @@ restaurants = [
     name: "La bonne poire",
     address: "5 rue de la jetée verte 75 001 Paris",
     phone_number: "+33 1 45 25 65 14",
-    category: "french"
+    category: "french",
+    picture: "http://40.media.tumblr.com/e61ac017d8f8c130d0f38acedd28f6eb/tumblr_ng9v03ynLQ1tomxvuo8_1280.jpg"
   },
     {
     name: "Le temps des cerises",
     address: "28 rue de la butte aux cailles 75 013 Paris",
     phone_number: "+33 1 64 89 25 16",
-    category: "french"
+    category: "french",
+    picture: "http://41.media.tumblr.com/fca0eb710f5acabffa1677a6c550dc70/tumblr_n1eimmwumj1tomxvuo6_1280.jpg"
   },
     {
     name: "La butte au Thai",
     address: "35 rues des cinq diamants 75 013 Paris",
     phone_number: "+33 9 14 25 89 45",
-    category: "thai"
+    category: "thai",
+    picture: "http://41.media.tumblr.com/5c94b465ed3d99adabf29cabc88e8820/tumblr_nmxa6wbRA71tomxvuo1_1280.jpg"
   },
   {
     name: "Pizza Pinaut",
     address: "18 grande rue 17 670 La Couarde sur mer",
     phone_number: "+33 9 14 25 89 45",
-    category: "thai"
+    category: "italian",
+    picture: "http://41.media.tumblr.com/ffc804c4c890f338e93b9187618b13a1/tumblr_n2uoyywZiU1tomxvuo7_1280.jpg"
   }
 ]
 
